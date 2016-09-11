0af1a1691d20c5a609d1eab3b25d2dc5a9928845
Charlie Burrows <charlie.burrows@gmail.com>
2016-09-11 09:42:20 -0400

    Refactored away from structured arrays to simple multidimensional ones

--- paint.py
+++ paint.py
@@ -41,22 +41,36 @@ def shuffle(grid):
                 elif lnk == j:
                     g['links'][li] = i
 
+POS=0
+GND=1
+GND_LEN=2
+LEN=0
+MAX_LEN=1
+X=0
+Y=1
+#@jit(nopython=True)
 def make_grid(x,y):
     gnd_max = 1.0
     gnd_min = 0.5
     len_mult_max = 1.5
     len_mult_min = 2.0
-    grid = np.zeros(x*y, dtype=struct_dtype)
-    for i, g in enumerate(grid):
-        g['pos'] = [i%x, i//x]
-        g['gnd_pos'] = [i%x, i//x]
-        g['gnd_len'] = 0.
-        g['gnd_max_len'] = gnd_min + random() * (gnd_max - gnd_min)
-        g['links'] = [-1]*nlinks
-        g['link_lens'] = [0]*nlinks
-        g['link_max_lens'] = [0]*nlinks
+    # grid[n][0] = pos
+    # grid[n][1] = gnd_pos
+    # grid[n][2][0] = gnd_len
+    # grid[n][2][1] = gnd_max_len
+    grid = np.zeros((x*y, 3, 2), dtype=np.float64)
+    links = np.zeros((x*y, nlinks), dtype=np.int64)
+    link_lens = np.zeros((x*y, nlinks, 2), dtype=np.float64)
+    for i in range(x*y):
+        g = grid[i]
+        g[POS] = [i%x, i//x]
+        g[GND] = [i%x, i//x]
+        g[GND_LEN][LEN] = 0.
+        g[GND_LEN][MAX_LEN] = gnd_min + random() * (gnd_max - gnd_min)
+        links[i] = [-1]*nlinks
+        link_lens[i] = [[0.,0.]]*nlinks
 
-    for i, g in enumerate(grid):
+    for i in range(x*y):
         targets = []
         if (i+1)%x != 0:
             targets.append(i+1)
@@ -66,22 +80,21 @@ def make_grid(x,y):
             targets.append(i+x+1)
         
         for it, target in enumerate(targets):
-            g['links'][it] = target
-            tdist2 = (g['pos'] - grid[target]['pos'])**2
-            g['link_lens'][it] = sqrt(tdist2[0] + tdist2[1])
-            g['link_max_lens'][it] = g['link_lens'][it] * (len_mult_min + random() * (len_mult_max - len_mult_min))
+            links[i][it] = target
+            tdist2 = (grid[i][POS] - grid[target][POS])**2
+            link_lens[i][it][LEN] = sqrt(tdist2[0] + tdist2[1])
+            link_lens[i][it][MAX_LEN] = link_lens[i][it][LEN] * (len_mult_min + random() * (len_mult_max - len_mult_min))
 
-    shuffle(grid)
+    return grid,links,link_lens
 
-    return grid
-
+#@jit(nopython=True)
 def expand_grid(grid):
     for g in grid:
-        g['gnd_pos'][0] *= 1.1
-        g['pos'][0] *= 1.1
+        g[GND][X] *= 1.1
+        g[POS][X] *= 1.1
 
-@jit(nopython=True)
-def iterate(grid, t=0.1, constraint_iterations=1):
+#@jit(nopython=True)
+def iterate(grid, links, link_lens, t=0.1, constraint_iterations=1):
     lcs = 0.30 # link constraint strength
     gcs = 0.99 # ground constraint strength
     lef = 1.00 # link expansion factor
@@ -95,72 +108,69 @@ def iterate(grid, t=0.1, constraint_iterations=1):
     while one_broke:
         # Apply constraints
         for _ in range(constraint_iterations):
-            for g in grid:
-                for lnk, lnk_len in zip(g['links'], g['link_lens']):
-                    if lnk == -1: continue
+            for g, glinks, glink_lens in zip(grid, links, link_lens):
+                for link, link_len in zip(glinks, glink_lens):
+                    if link == -1: continue
 
-                    d = g['pos'] - grid[lnk]['pos']
+                    d = g[POS] - grid[link][POS]
                     dmag = sqrt(d[0]**2 + d[1]**2)
                     if dmag > 0.:
-                        mv = lcs * 0.5 * (dmag - lnk_len) * d / dmag
-                        g['pos'][0] -= mv[0]
-                        g['pos'][1] -= mv[1]
-                        grid[lnk]['pos'][0] += mv[0]
-                        grid[lnk]['pos'][1] += mv[1]
+                        mv = lcs * 0.5 * (dmag - link_len) * d / dmag
+                        g[POS] -= mv
+                        grid[link][POS] += mv
 
             for g in grid:
                 # Ground linkage
-                if g['gnd_len'] > -1: # not broken
-                    d = g['pos'] - g['gnd_pos']
+                if g[GND_LEN][LEN] > -1: # not broken
+                    d = g[POS] - g[GND]
                     dmag = sqrt(d[0]**2 + d[1]**2)
                     if dmag > 0.:
-                        mv = gcs * (dmag - g['gnd_len']) * d / dmag
-                        g['pos'][0] -= mv[0]
-                        g['pos'][1] -= mv[1]
+                        mv = gcs * (dmag - g[GND_LEN][LEN]) * d / dmag
+                        g[POS] -= mv
 
         # calculate the strain and adjust link length for all the links
         # If we get through all the length adjustments without a break then we
         # can exit the procedure
         one_broke = False
-        for g in grid:
+        for g, glinks, glink_lens in zip(grid, links, link_lens):
             if one_broke: break
-            for i, (lnk, lnk_len, lnk_max_len) in enumerate(zip(g['links'], g['link_lens'], g['link_max_lens'])):
-                if lnk == -1: continue
+            for i, (link, link_len) in enumerate(zip(glinks, glink_lens)):
+                if link == -1: continue
 
-                d = g['pos'] - grid[lnk]['pos']
+                d = g[POS] - grid[link][POS]
                 dmag = sqrt(d[0]**2 + d[1]**2)
-                lnk_len += lef * (dmag - lnk_len)
+                link_len[LEN] += lef * (dmag - link_len[LEN])
 
-                if lnk_len > lnk_max_len:
+                if link_len[LEN] > link_len[MAX_LEN]:
                     # remove link
-                    g['links'][i] = -1
+                    glinks[i] = -1
                     one_broke = True
                     break
 
-            if g['gnd_len'] > -1: # not broken
-                d = g['pos'] - g['gnd_pos']
+            if g[GND_LEN][LEN] > -1: # not broken
+                d = g[POS] - g[GND]
                 dmag = sqrt(d[0]**2 + d[1]**2)
-                g['gnd_len'] += gef * (dmag - g['gnd_len'])
+                g[GND_LEN][LEN] += gef * (dmag - g[GND_LEN][LEN])
 
-                if g['gnd_len'] > g['gnd_max_len']:
+                if g[GND_LEN][LEN] > g[GND_LEN][MAX_LEN]:
                     # remove link
-                    g['gnd_len'] = -1
+                    g[GND_LEN][LEN] = -1
                     one_broke = True
                     break
 
-def draw(grid, frame):
+def draw(grid, links, frame):
     dwg = svg.Drawing('test%05d.svg'%frame)
     minx = miny =  9999999
     maxx = maxy = -9999999
-    for g in grid:
-        minx = g['pos'][0] if g['pos'][0] < minx else minx
-        maxx = g['pos'][0] if g['pos'][0] > maxx else maxx
-        miny = g['pos'][1] if g['pos'][1] < miny else miny
-        maxy = g['pos'][1] if g['pos'][1] > maxy else maxy
+    for g, glinks in zip(grid, links):
+        minx = g[POS][0] if g[POS][0] < minx else minx
+        maxx = g[POS][0] if g[POS][0] > maxx else maxx
+        miny = g[POS][1] if g[POS][1] < miny else miny
+        maxy = g[POS][1] if g[POS][1] > maxy else maxy
 
-        for lnk in [k for k in g['links'] if k != -1]:
-            line = svg.shapes.Line((g['pos'][0], g['pos'][1]),
-                                    (grid[lnk]['pos'][0], grid[lnk]['pos'][1]),
+        for lnk in [k for k in glinks if k != -1]:
+            line = svg.shapes.Line((g[POS][0], g[POS][1]),
+                                    (grid[lnk][POS][0], grid[lnk][POS][1]),
                                     stroke='black',
                                     stroke_width=0.1)
             dwg.add(line)
@@ -170,19 +180,19 @@ def draw(grid, frame):
 
     dwg.save()
 
-def draw2(grid, frame):
+def draw2(grid, links, frame):
     dwg = svg.Drawing('test%05d.svg'%frame)
     minx = miny =  9999999
     maxx = maxy = -9999999
-    for g in grid:
-        minx = g['pos'][0] if g['pos'][0] < minx else minx
-        maxx = g['pos'][0] if g['pos'][0] > maxx else maxx
-        miny = g['pos'][1] if g['pos'][1] < miny else miny
-        maxy = g['pos'][1] if g['pos'][1] > maxy else maxy
+    for g, glinks in zip(grid, links):
+        minx = g[POS][0] if g[POS][0] < minx else minx
+        maxx = g[POS][0] if g[POS][0] > maxx else maxx
+        miny = g[POS][1] if g[POS][1] < miny else miny
+        maxy = g[POS][1] if g[POS][1] > maxy else maxy
 
-        for lnk in [k for k in g['links'] if k != -1]:
-            p = (g['pos'] + grid[lnk]['pos']) / 2.
-            d = g['pos'] - grid[lnk]['pos']
+        for lnk in [k for k in glinks if k != -1]:
+            p = (g[POS] + grid[lnk][POS]) / 2.
+            d = g[POS] - grid[lnk][POS]
             d = d**2
             r = sqrt(np.sum(d)) / 2.
             line = svg.shapes.Circle((p[0], p[1]), r,
@@ -211,17 +221,19 @@ def main():
             load_fn = arg
 
     if load_fn != '':
-        grid = np.load(load_fn)
-        draw2(grid, -1)
+        arys = np.loadz(load_fn)
+        grid, links, link_lens = arys['grid'], arys['links'], arys['link_lens']
+        draw2(grid, links, -1)
     else:
-        x,y = 100,100
-        grid = make_grid(x,y)
-        draw(grid, -1)
+        x,y = 10,10
+        grid, links, link_lens = make_grid(x,y)
+        #shuffle(grid)
+        draw(grid, links, -1)
         for i in range(10):
             expand_grid(grid)
-            iterate(grid)
-            draw(grid, i)
-            np.save(save_fn + '%05d.npy'%i, grid)
+            iterate(grid, links, link_lens)
+            draw(grid, links, i)
+            np.savez(save_fn + '%05d.npy'%i, grid=grid, links=links, link_lens=link_lens)
             print('.', end='')
             sys.stdout.flush()
         print()
